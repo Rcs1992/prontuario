d70b542b7b820aa237dead64c99cfc6cf400b3c7
Rcs1992 <rodrigo-cavalcante-santos@hotmail.com>
2025-07-30 16:56:29 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAh4cDk7h-x0IekJyQEvP4FUbHds6z7Dtc",
+  authDomain: "passagem-plantao2025.firebaseapp.com",
+  projectId: "passagem-plantao2025",
+  storageBucket: "passagem-plantao2025.firebasestorage.app",
+  messagingSenderId: "46390981643",
+  appId: "1:46390981643:web:158251408fe14ac4ba030e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
